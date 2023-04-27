0ff38e20a99123ea9f877e68ade54024ecc88994
stroich <stroichmwork@gmail.com>
2023-04-27 18:06:05 +0400

    feat: add language

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 //the generation of DOM elements is implemented - start
+let language = 'en';
 let keyboard = [
     [
-        ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+        {
+            'en':['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+            'ru':['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace']
+        },
         ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace']
     ],
     [
-        ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\','Delete'],
+        {
+            'en':['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\','Delete'],
+            'ru':['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Delete']
+        },
         ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash','Delete']
     ],
     [
-        ['CapsLock','a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter'],
+        {
+            'en':['CapsLock','a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter'],
+            'ru':['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter']
+        },
         ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter']
     ],
     [
-        ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'Shift'],
+        {
+            'en':['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'Shift'],
+            'ru':['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '▲', 'Shift']
+        },
         ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight']
         ],
     [
-        ['Ctrl','Win','Alt',' ','Alt','◄','▼','►','Ctrl'],
+        {
+            'en':['Ctrl','Win','Alt',' ','Alt','◄','▼','►','Ctrl'],
+            'ru':['Ctrl','Win','Alt',' ','Alt','◄','▼','►','Ctrl']
+        },
         ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight']
     ]
 ];
@@ -34,12 +50,16 @@ function generatingElements(){
         <div class="virtual-keyboard-row"></div>
         <div class="virtual-keyboard-row"></div>
       </div>
+      <div class="text">
+        <p class="text-element">Клавиатура создана в операционной системе Windows</p>
+        <p class="text-element">Для переключения языка комбинация: левыe shift + alt</p>
+       </div>
     </div>`;
     let body = document.querySelector('.body');
     body.insertAdjacentHTML('afterbegin', container);
     let containerKeyboard = document.querySelectorAll('.virtual-keyboard-row');
     for (let i=0; i<5; i++){
-        keyboard[i][0].forEach((el,ind)=>{
+        keyboard[i][0][language].forEach((el,ind)=>{
             let elementKeyboard = document.createElement('div');
             elementKeyboard.classList.add('virtual-keyboard-element');
             elementKeyboard.dataset.code = keyboard[i][1][ind];
@@ -67,6 +87,29 @@ function generatingElements(){
 }
 generatingElements();
 //the generation of DOM elements is implemented - end
+//language start
+function changeLanguage(){
+    let rowsKeyboard = document.querySelectorAll('.virtual-keyboard-row');
+    rowsKeyboard.forEach((el,ind)=>{
+        let elementKeyboard = el.querySelectorAll('.virtual-keyboard-element');
+        elementKeyboard.forEach((element,index)=>{
+            element.innerHTML = keyboard[ind][0][language][index]
+        })
+    })
+
+}
+function setLocalStorage() {
+    localStorage.setItem('language', language);
+}
+window.addEventListener('beforeunload', setLocalStorage);
+function getLocalStorage() {
+    if(localStorage.getItem('language')) {
+        language = localStorage.getItem('language');
+        changeLanguage();
+    }
+}
+window.addEventListener('load', getLocalStorage)
+//language end
 // pressing a key on a physical keyboard highlights the key on the virtual keyboard -start
 
 document.addEventListener('keydown',(event)=>{
@@ -76,6 +119,15 @@ document.addEventListener('keydown',(event)=>{
             el.classList.add('active');
         }
     })
+    if (event.code==='ShiftLeft' && event.altKey || event.code==='AltLeft' && event.shiftKey){
+        if(language==='en'){
+            language ='ru';
+            changeLanguage();
+        }else{
+            language='en';
+            changeLanguage();
+        }
+    }
 })
 document.addEventListener('keyup',(event)=>{
     let elementKeyboard = document.querySelectorAll('.virtual-keyboard-element');
@@ -85,4 +137,5 @@ document.addEventListener('keyup',(event)=>{
         }
     })
 })
-// pressing a key on a physical keyboard highlights the key on the virtual keyboard -start
+// pressing a key on a physical keyboard highlights the key on the virtual keyboard -end
+
