cc8f1ea42629de2e9bf71b64a887569a1ca0f4ca
stroich <stroichmwork@gmail.com>
2023-04-27 16:04:49 +0400

    feat: implement a keystroke on a physical keyboard highlights a key on a virtual keyboard

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 //the generation of DOM elements is implemented - start
 let keyboard = [
-    ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
-    ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\','Delete'],
-    ['CapsLock','a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter'],
-    ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'Shift'],
-    ['Ctrl','Win','Alt',' ','Alt','◄','▼','►','Ctrl']
+    [
+        ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+        ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace']
+    ],
+    [
+        ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\','Delete'],
+        ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash','Delete']
+    ],
+    [
+        ['CapsLock','a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter'],
+        ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter']
+    ],
+    [
+        ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'Shift'],
+        ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight']
+        ],
+    [
+        ['Ctrl','Win','Alt',' ','Alt','◄','▼','►','Ctrl'],
+        ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight']
+    ]
 ];
 
 function generatingElements(){
@@ -24,9 +39,10 @@ function generatingElements(){
     body.insertAdjacentHTML('afterbegin', container);
     let containerKeyboard = document.querySelectorAll('.virtual-keyboard-row');
     for (let i=0; i<5; i++){
-        keyboard[i].forEach(el=>{
+        keyboard[i][0].forEach((el,ind)=>{
             let elementKeyboard = document.createElement('div');
             elementKeyboard.classList.add('virtual-keyboard-element');
+            elementKeyboard.dataset.code = keyboard[i][1][ind];
             elementKeyboard.innerHTML = el;
             containerKeyboard[i].append(elementKeyboard);
         })
@@ -51,3 +67,22 @@ function generatingElements(){
 }
 generatingElements();
 //the generation of DOM elements is implemented - end
+// pressing a key on a physical keyboard highlights the key on the virtual keyboard -start
+
+document.addEventListener('keydown',(event)=>{
+    let elementKeyboard = document.querySelectorAll('.virtual-keyboard-element');
+    elementKeyboard.forEach(el=>{
+        if (event.code===el.dataset.code){
+            el.classList.add('active');
+        }
+    })
+})
+document.addEventListener('keyup',(event)=>{
+    let elementKeyboard = document.querySelectorAll('.virtual-keyboard-element');
+    elementKeyboard.forEach(el=>{
+        if (event.code===el.dataset.code){
+            el.classList.remove('active');
+        }
+    })
+})
+// pressing a key on a physical keyboard highlights the key on the virtual keyboard -start
